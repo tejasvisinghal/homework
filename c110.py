62ccf980288a2a7b7e4435f8ee677d456d22ce91
tejasvisinghal <122531023+tejasvisinghal@users.noreply.github.com>
2023-01-12 17:00:48 +0530

    Add files via upload

---
+++ c110.py
@@ -0,0 +1,32 @@
+#import time
+#print(dir(time))
+#seconds=input("ENTER THE TIME IN NO. OF SECONDS")
+#def counter(seconds):
+ #   while seconds>0:
+  #      mins=int(seconds/60)
+   #     secs=int(seconds%60)
+    #    timer = f'{mins}:{secs}'
+     #   print(timer)
+      #  time.sleep(2)
+       # seconds-=1
+   # print("TIMES UP!")
+#counter(int(seconds))
+
+#homework
+
+import random
+
+random=input("DO YOU WANT TO ROLL DICE ?")
+print("PRESS value of a as 10 TO CONTINUE and value of b as 5  otherwise enter a greater value of b")
+
+a= input("enter the value of a")
+b= input("enter the value of b")
+if a>b:
+  print("gin")
+else:
+    print("[-------]")
+print("[0]")
+print("[]")
+print("[-------]")
+
+
